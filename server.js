60a21f664feda92df9da020a80b473bc5f799b34
AleFaccio5805 <faccioalessandro2005@gmail.com>
2023-10-13 09:44:25 +0200

    Creazione headers della risposta

--- server.js
+++ server.js
@@ -6,4 +6,18 @@
 
 const http = require("http");
 
-var server = http.createServer();
+/*
+    1° parametro => richiesta del client
+    2° parametro => risposta per il client
+*/
+
+var server = http.createServer(function(richiesta, risposta){
+    /*
+        INTESTAZIONE DELLA RISPOSTA (HEADERS)
+        1° parametro => codice di risposta (200/400/501 ...)
+        2° parametro => oggetto JSON, insieme di opzioni da inserire nell'intestazione
+    */
+    let header = {"Content-Typeb" : "text/plain"};
+
+    risposta.writeHead(200, header);
+});
